1ed15363eb861a58d4d4e6e48bb34ea89b633ad0
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2022-02-09 17:30:19 +0300

    Ürün listesinde silme ekleme güncelleme işlemleri tamamlandı

--- TeknikServisWinUI/Formlar/FrmUrunListesi.cs
+++ TeknikServisWinUI/Formlar/FrmUrunListesi.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DevExpress.Utils.Design;
 
 namespace TeknikServisWinUI.Formlar
 {
@@ -15,6 +16,87 @@ namespace TeknikServisWinUI.Formlar
         public FrmUrunListesi()
         {
             InitializeComponent();
+        }
+
+        private DbTeknikServisEntities db = new DbTeknikServisEntities(); //teknik servis sınıfı
+
+        void UrunListele()
+        {
+            //var degerler = db.TBLURUN.ToList();
+            var degerler = from u in db.TBLURUN
+            select new
+        {
+            u.ID,
+            u.AD,
+            u.MARKA,
+            KATEGORI = u.TBLKATEGORI.AD,
+            u.STOK,
+            u.ALISFIYAT,
+            u.SATISFIYAT
+        };
+    gridControl1.DataSource = degerler.ToList();
+        }
+
+
+        private void FrmUrunListesi_Load(object sender, EventArgs e)
+        {
+            UrunListele();
+            lookUpEdit1.Properties.DataSource = db.TBLKATEGORI.ToList(); //BU ALANI EKLERSEN ID VE ADLARI LİSTELER
+        }
+
+        private void BtnKaydet_Click(object sender, EventArgs e)
+        {
+            TBLURUN t = new TBLURUN();
+            t.AD = TxtUrunAd.Text;
+            t.MARKA = TxtMarka.Text;
+            t.ALISFIYAT = decimal.Parse(TxtAlisFiyat.Text);
+            t.SATISFIYAT = decimal.Parse(TxtSatisFiyat.Text);
+            t.STOK = short.Parse(TxtStok.Text);
+            t.DURUM = false;
+            t.KATEGORI = byte.Parse(lookUpEdit1.EditValue.ToString());
+            db.TBLURUN.Add(t);
+            db.SaveChanges();//değişiklikleri kaydet
+            MessageBox.Show("Ürün Başarıyla Kaydedildi","Bilgi",MessageBoxButtons.OK,MessageBoxIcon.Information);
+
+        }
+
+        private void BtnListele_Click(object sender, EventArgs e)
+        {
+            UrunListele();
+        }
+
+        private void gridView1_FocusedRowChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs e)
+        {
+            //odaklanan değeri almaya yarar
+            TxtUrunId.Text = gridView1.GetFocusedRowCellValue("ID").ToString();
+            TxtUrunAd.Text = gridView1.GetFocusedRowCellValue("AD").ToString();
+            TxtMarka.Text = gridView1.GetFocusedRowCellValue("MARKA").ToString();
+            TxtAlisFiyat.Text = gridView1.GetFocusedRowCellValue("ALISFIYAT").ToString();
+            TxtSatisFiyat.Text = gridView1.GetFocusedRowCellValue("SATISFIYAT").ToString();
+            TxtStok.Text = gridView1.GetFocusedRowCellValue("STOK").ToString();
+        }
+
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(TxtUrunId.Text);
+            var deger = db.TBLURUN.Find(id); //değeri bul ata
+            db.TBLURUN.Remove(deger);
+            db.SaveChanges();
+            MessageBox.Show("Ürün Başarıyla Silindi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+        }
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(TxtUrunId.Text);
+            var deger = db.TBLURUN.Find(id);
+            deger.AD = TxtUrunAd.Text;
+            deger.STOK = short.Parse(TxtStok.Text);
+            deger.MARKA = TxtMarka.Text;
+            deger.ALISFIYAT = decimal.Parse(TxtAlisFiyat.Text);
+            deger.SATISFIYAT = decimal.Parse(TxtSatisFiyat.Text);
+            deger.KATEGORI = byte.Parse(lookUpEdit1.EditValue.ToString());
+            db.SaveChanges();
+            MessageBox.Show("Ürün Başarıyla Güncellendi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
         }
     }
 }
